809fc3c98198fa022b9c6d23bd927caa32e60c4f
Manuela Ozán <manuelaozaan@hotmail.com>
2023-10-30 23:54:57 -0300

    backend logic

---
+++ back/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.post("/paraphrase", async (req, res) => {
+  const inputText = req.body.input;
+  try {
+    const response = await axios.post(
+      "https://paraphraser1.p.rapidapi.com/",
+      {
+        input: inputText,
+      },
+      {
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "283cb8db9dmsh28e27de947a855dp17a818jsn88b265dc15f3",
+          "X-RapidAPI-Host": "paraphraser1.p.rapidapi.com",
+        },
+      }
+    );
+
+    const paraphrasedText = response.data.output;
+    res.json({ paraphrasedText });
+  } catch (error) {
+    res.status(500).json({ error: "Error al parafrasear el texto." });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
+});
